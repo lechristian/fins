d1a92dc09c319b3f145e098a3b087d64c5ee717b
Corey Short <cshort012@gmail.com>
2015-03-18 14:36:54 -0700

    Working code base.

---
+++ flip/app/src/main/java/prov2/flip/MainActivity.java
@@ -0,0 +1,46 @@
+package prov2.flip;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+
+public class MainActivity extends Activity {
+
+    private static final int REQUEST_SELECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+    private static final int UART_PROFILE_READY = 10;
+    public static final String TAG = "flip";
+    private static final int UART_PROFILE_CONNECTED = 20;
+    private static final int UART_PROFILE_DISCONNECTED = 21;
+    private static final int STATE_OFF = 10;
+
+    private Button connectBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+
+    }
+
+
+    public void DeviceScanActivity(View view) {
+
+    }
+
+
+}
